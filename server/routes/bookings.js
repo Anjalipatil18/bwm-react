2953a8e2a22c561038139a74ea92bbd01ce5c0eb
Anjalipatil18 <anjalip18@navgurukul.org>
2020-06-29 23:43:52 +0530

    Adds manage rental booking endpoints rental endpoint

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -5,4 +5,7 @@ const BookingCtrl=require('../controller/booking');
 
 router.post('',UserCtrl.authMiddleware,BookingCtrl.createBooking);
 
+router.get('/manage', UserCtrl.authMiddleware, BookingCtrl.getUserBookings);
+
+
 module.exports=router;
--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -12,6 +12,24 @@ router.get('/secret',UserCtrl.authMiddleware,function(req,res){
 })
 
 
+router.get('/manage',  UserCtrl.authMiddleware, function(req, res) {
+  const user = res.locals.user;
+
+  Rental
+    .where({user})
+    .populate('bookings')
+    .exec(function(err, foundRentals) {
+
+    if (err) {
+      return res.status(422).send({errors: normalizeErrors(err.errors)});
+    }
+
+    return res.json(foundRentals);
+  });
+});
+
+
+
 router.get('/:id',(req,res)=>{
     const rentalId=req.params.id;
 
@@ -26,6 +44,43 @@ router.get('/:id',(req,res)=>{
           
         res.json(foundRental);
         });          
+});
+
+
+
+router.delete('/:id', UserCtrl.authMiddleware, function(req, res) {
+  const user = res.locals.user;
+
+  Rental
+    .findById(req.params.id)
+    .populate('user', '_id')
+    .populate({
+      path: 'bookings',
+      select: 'startAt',
+      match: { startAt: { $gt: new Date()}}
+    })
+    .exec(function(err, foundRental) {
+
+    if (err) {
+      return res.status(422).send({errors: normalizeErrors(err.errors)});
+    }
+
+    if (user.id !== foundRental.user.id) {
+      return res.status(422).send({errors: [{title: 'Invalid User!', detail: 'You are not rental owner!'}]});
+    }
+
+    if (foundRental.bookings.length > 0) {
+      return res.status(422).send({errors: [{title: 'Active Bookings!', detail: 'Cannot delete rental with active bookings!'}]});
+    }
+
+    foundRental.remove(function(err) {
+      if (err) {
+        return res.status(422).send({errors: normalizeErrors(err.errors)});
+      }
+
+      return res.json({'status': 'deleted'});
+    });
+  });
 });
 
 router.post('', UserCtrl.authMiddleware, function(req, res) {
